7c1d213479a9142cfca639184cb56e2e1b4fa291
典 <80772052+edwardkuo09@users.noreply.github.com>
2022-05-21 13:30:44 +0800

    Add files via upload

---
+++ apilesson/apilesson/Controllers/datacontroller.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apilesson.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class datacontroller : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _env;
+        public datacontroller(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            _configuration = configuration;
+            _env = env;
+
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                        select uid,people ,place,wet,tmp
+                        from 
+                        data.test
+            ";
+//ORDER BY uid DESC LIMIT 1
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("lessontestcon");
+            MySqlDataReader myReader;
+            using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    mycon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+    }
+}
---
+++ apilesson/apilesson/data/data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apilesson.data
+{
+    public class data
+    {
+        public int uid { get; set; }
+        public int place { get; set; }
+        public int people { get; set; }
+        public double tmp { get; set; }
+        public double wet { get; set; }
+
+}
+}
